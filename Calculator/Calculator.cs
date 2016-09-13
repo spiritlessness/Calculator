547a1a3d1092603a3c5e48db9f8b2ec6a210f9b5
Luchina Yauheni <triptofun1507@gmail.com>
2016-09-13 14:18:36 +0300

    base functionality calculator

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public static double Divide(double first, double second)
+        {
+            try
+            {
+                if (second == 0) throw (new Exception("Dividing by zero"));
+                return first / second;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return -1;
+        }
+        public static double Multiplier(double first, double second)
+        {
+                return first * second;
+            }
+        public static double Add(double first, double second)
+            {
+                return first + second;
+            }
+        public static double Minus(double first, double second)
+            {
+              return first - second;
+            }
+    }
+}
